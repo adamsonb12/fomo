e1849d9d6b6e81217a0f53d7be2346348648d482
Brett Adamson <adamsonb12@gmail.com>
2017-02-25 00:50:54 -0700

    login form showing but the login function isn't working at all

--- fomo/account/views/login.py
+++ fomo/account/views/login.py
@@ -1,21 +1,49 @@
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django_mako_plus import view_function
 from django.contrib.auth import authenticate, login
+from datetime import datetime
+from account import models as amod
+from .. import dmp_render, dmp_render_to_string
+from formlib.form import FormMixIn
+from django import forms
 
 @view_function
 def process_request(request):
-	#authenticate the user
-	username = 'user1'
-	password = 'password'
 
-	user = authenticate(username=username, password=password)
-	if user is not None:
-	    login(request,user)
+    # process the form
+	form = LoginForm(request)
+	if form.is_valid():
+	    form.commit()
+	    return HttpResponseRedirect('/manager/users/')
 
-		# return HttpResponseRedirect('/account/index/')
+	context = {
+	    'form': form,
+	}
+	return dmp_render(request, 'login.html', context)
 
-	# return HttpResponseRedirect('/')
+class LoginForm(FormMixIn, forms.Form):
+	
+	def init(self):
+	    self.fields['username'] = forms.CharField(label='Username', max_length=100)
+	    self.fields['password'] = forms.CharField(label='Password', max_length=100)
 
-	return HttpResponseRedirect('/account/index')
-	    
+	def commit(self):
+
+		un = self.cleaned_data.get('username')
+		ps = self.cleaned_data.get('password')
+
+		user = authenticate(username=un, password=ps)
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect('/manager/users')
+		else:
+			return HttpResponseRedirect('/')
+
+
+
+
+
+
+
+
