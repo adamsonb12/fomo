229a10a892a464e19991540935907bc446241f6c
Brett Adamson <adamsonb12@gmail.com>
2017-03-09 15:15:32 -0700

    middleware working

--- fomo/fomo/middleware.py
+++ fomo/fomo/middleware.py
@@ -3,10 +3,12 @@ def Last5ProductsMiddleware(get_response):
 
     def middleware(request):
         # Code to be executed for each request before
+        print('2222222222222222222 Begin Middle Ware')
         # the view (and later middleware) are called.
         request.last5 = request.session.get('last5')
         if request.last5 is None:
         	request.last5 = []
+        request.last5 = request.last5
 
  
         # let Django continue the processing and calls view function
@@ -14,6 +16,7 @@ def Last5ProductsMiddleware(get_response):
 
         # Code to be executed for each request/response after
         # the view is called.
+        print('2222222222222222222 end Middle Ware')
         request.session['last5'] = request.last5[:5 ]
         return response
 
